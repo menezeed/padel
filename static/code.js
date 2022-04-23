7d9b5b91d92fea6e2bc901b3baea7188dff54bda
Eduardo Menezes <eduardomenezes.br@gmail.com>
2022-04-23 12:36:58 +0100

    'init'

---
+++ static/code.js
@@ -0,0 +1,19 @@
+angular.module("MyApp", ['ngResource']);
+
+angular.module('MyApp').controller("MyControler", function($scope, $resource, $timeout, $sce){
+    var arrarypedal = [];
+
+    $scope.refresh = function(){
+        $resource('/pedal').get().$promise.then(function(result) {
+            $scope.result = result;
+            $scope.arrarypedal = arrarypedal;
+        }).catch(function(error){
+            //empty
+        }).finally(function(){
+            $timeout($scope.refresh, 30000);
+        });
+    };
+
+    $scope.refresh();
+
+});
