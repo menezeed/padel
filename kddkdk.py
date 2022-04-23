26d3a4f5ecdb77d4e8b97f5da2891f355ccc480d
Eduardo Menezes <eduardomenezes.br@gmail.com>
2022-04-23 18:50:45 +0100

    Fixing using pyodbc instead of mysql.connector

--- kddkdk.py
+++ kddkdk.py
@@ -7,7 +7,7 @@ from flask import redirect
 from flask import Flask
 application = Flask(__name__)
 
-database = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER=localhost;DATABASE=srv_padel_uat;UID=root;PWD=London@990'
+database = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER=localhost;DATABASE=srv_padel_uat;UID=root;PWD=A**##770'
 
 base_path = 'http://127.0.0.1:5000'
 
