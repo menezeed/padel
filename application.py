9d16403acff355b2d86f266969f1786de26ab47e
Eduardo Menezes <eduardomenezes.br@gmail.com>
2022-04-23 14:18:45 +0100

    Fixing using nothing44r

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ application = Flask(__name__)
 
 database = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER=localhost;DATABASE=srv_padel_uat;UID=root;PWD=London@990'
 
-base_path = 'http://flaskexample-env.eba-aaetkm9m.us-east-2.elasticbeanstalk.com/'
+base_path = 'http://flaskexample-env.eba-aaetkm9m.us-east-2.elasticbeanstalk.com'
 
 
 
