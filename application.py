e40455cd603ffc12303665d566e89b5cd9cd0ae3
Eduardo Menezes <eduardomenezes.br@gmail.com>
2022-04-23 16:46:01 +0100

    Fixing usinr

--- application.py
+++ application.py
@@ -11,3 +11,22 @@ base_path = 'http://127.0.0.1:5000'
 @application.route('/')
 def root():
     return 'test'
+
+
+    
+@application.route('/pedal', methods=['GET'])
+def report_padelmatch():
+
+    
+    result_pedal = []
+    result_pedal.append({
+            "player": 1,
+            "points": 2
+        })
+    
+    return jsonify({
+        "pedal": result_pedal
+    })
+
+if __name__=="__main__":
+    application.run
